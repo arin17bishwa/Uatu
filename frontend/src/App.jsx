ddf831fdaf3ee453b725ff3dfb08570a9cb92530
bishwajit <arin17bishwa@gmail.com>
2025-05-03 15:50:01 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind!</h1>
+    </div>
+  )
+}
+
+export default App;
